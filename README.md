fbb9a4730b262d64dc597b91e4ddfa1e00367c4d
qiwang1996 <1290220814@qq.com>
2022-06-12 17:04:52 +0800

    v2

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # Memory-Tuning
 Code for NeuralPS 2022 submission titled "Memory-Tuning: A Unified Parameter-Efficient Tuning Method for Pre-trained Language Models "
+
+
+## Main experiment and Robustness Analysis  
+
+script.sh supports 7 tuning methods  for training and  inferring on 8 dataset used in paper.
+There are  line command examples running on 3  datasets  in given script.sh. You can add the remaining
+line commands for other datasets according to those given ones and please note different setup for different dataset in Table 1 of our paper.
+ 
+
+## Visualization for attention vector  of  memory slots  
+
+Setup of our released code is setted for using BERT-Large but our visualization experiment is conducted  by using BERT-base. So you need to revise several
+places in code for visualization experiment using BERT-base.  
+
+Step1: In ffn_trainabl e_module/bert.py  line 120-121,   set  n_layer=12 n_head=12,  n_embd=768, mid_dim=1024  
+Step2:   
+Step3: use  predict.py  for inferring  
+Step4:  set   --test-file './data/SST-2/dev.tsv'    --test-file './data/MRPC/dev.tsv'  in line command for SST-2 and MRPC respectively.  (Since we experiment on  the dev file)  
+Step5: copy the inferred attention data and  pred.txt  into Visualize\MSRA\memory-ffn,  Run compare.txt and then Run t-sne.py  
+
+ 
+If you have any question,  feel free to connect me by sending an email into 1290220814@qq.com.  :)
+
+
--- script.sh
+++ script.sh
@@ -1,23 +1,25 @@
 source activate qw
 
-
 cd  Sentence-Level
 
+#an example for  runing on CB dataset  
 python3 main.py  --mode 'ft'  --train-file './data/CB/train.jsonl' --val-file './data/CB/val.jsonl'  --test-file './data/CB/test.jsonl'   --saved-file  './predict/ft/CB.jsonl'   --task 'CB'  --lr 2e-5   --batch-size 16   --max-length 300   
-
 python3 main.py  --mode 'bitfit'  --train-file './data/CB/train.jsonl' --val-file './data/CB/val.jsonl'  --test-file './data/CB/test.jsonl'   --saved-file  './predict/bitfit/CB.jsonl'   --task 'CB'    --lr 1e-4   --batch-size 16     --max-length 300  
-
 python3 main.py  --mode 'prefix'  --train-file './data/CB/train.jsonl' --val-file './data/CB/val.jsonl'  --test-file './data/CB/test.jsonl'   --saved-file  './predict/prefix/CB.jsonl'   --task 'CB'  --preseqlen 32    --lr 1e-4   --batch-size 16  --max-length 300   
-
 python3 main.py  --mode 'adapter'  --train-file './data/CB/train.jsonl' --val-file './data/CB/val.jsonl'  --test-file './data/CB/test.jsonl'   --saved-file  './predict/adapter/CB.jsonl'   --task 'CB'    --random-seed 62  --lr 1e-4   --batch-size 16     --max-length 300  --ffn-module-size 32   
-
 python3 main.py  --mode 'memory-ffn'  --train-file './data/CB/train.jsonl' --val-file './data/CB/val.jsonl'  --test-file './data/CB/test.jsonl'   --saved-file  './predict/memory-ffn/CB.jsonl'   --task 'CB'   --lr 1e-4   --batch-size 16     --max-length 300  --ffn-module-size 32  --memory-num 50    
-
-
 python3 main.py  --mode 'memory'  --train-file './data/CB/train.jsonl' --val-file './data/CB/val.jsonl'  --test-file './data/CB/test.jsonl'   --saved-file  './predict/memory/CB.jsonl'   --task 'CB'  --lr 1e-4   --batch-size 16     --max-length 300  --ffn-module-size 16  --memory-num 50  --preseqlen 16
-
-
 python3 main.py  --mode 'mam_adapter'  --train-file './data/CB/train.jsonl' --val-file './data/CB/val.jsonl'  --test-file './data/CB/test.jsonl'   --saved-file  './predict/mam_adapter/CB.jsonl'   --task 'CB'   --lr 1e-4  --max-length 300   --batch-size 16   --preseqlen 16  --ffn-module-size 16   
+
+
+#an example for  runing on  MRPC dataset 
+python3 main.py  --mode 'ft'  --train-file './data/MRPC/train.tsv' --val-file './data/MRPC/dev.tsv'  --test-file './data/MRPC/test.tsv'   --saved-file  './predict/ft/MRPC.tsv'   --task 'MRPC'  --lr 2e-5   --batch-size 16      
+python3 main.py  --mode 'bitfit'  --train-file './data/MRPC/train.tsv' --val-file './data/MRPC/dev.tsv'  --test-file './data/MRPC/test.tsv'   --saved-file  './predict/ft/MRPC.tsv'   --task 'MRPC'    --lr 1e-4   --batch-size 16        
+python3 main.py  --mode 'prefix'  --train-file './data/MRPC/train.tsv' --val-file './data/MRPC/dev.tsv'  --test-file './data/MRPC/test.tsv'   --saved-file  './predict/ft/MRPC.tsv'   --task 'MRPC'  --preseqlen 32    --lr 1e-4   --batch-size 16  
+python3 main.py  --mode 'adapter'   --train-file './data/MRPC/train.tsv' --val-file './data/MRPC/dev.tsv'  --test-file './data/MRPC/test.tsv'   --saved-file  './predict/ft/MRPC.tsv'   --task 'MRPC'    --random-seed 62  --lr 1e-4   --batch-size 16   --ffn-module-size 32   
+python3 main.py  --mode 'memory-ffn'    --train-file './data/MRPC/train.tsv' --val-file './data/MRPC/dev.tsv'  --test-file './data/MRPC/test.tsv'   --saved-file  './predict/ft/MRPC.tsv'   --task 'MRPC'    --lr 1e-4   --batch-size 16    --ffn-module-size 32  --memory-num 50    
+python3 main.py  --mode 'memory'    --train-file './data/MRPC/train.tsv' --val-file './data/MRPC/dev.tsv'  --test-file './data/MRPC/test.tsv'   --saved-file  './predict/ft/MRPC.tsv'   --task 'MRPC'    --lr 1e-4   --batch-size 16     --ffn-module-size 16  --memory-num 50  --preseqlen 16
+python3 main.py  --mode 'mam_adapter'    --train-file './data/MRPC/train.tsv' --val-file './data/MRPC/dev.tsv'  --test-file './data/MRPC/test.tsv'   --saved-file  './predict/ft/MRPC.tsv'   --task 'MRPC'   --lr 1e-4    --batch-size 16   --preseqlen 16  --ffn-module-size 16   
 
 
 
@@ -27,16 +29,9 @@ python3 main.py  --mode 'mam_adapter'  --train-file './data/CB/train.jsonl' --va
 cd Token-Level
 
 python3 main.py --mode 'prefix' --train-file './data/msra/train.txt' --val-file './data/msra/valid.txt'  --test-file './data/msra/test.txt'   --task 'msra'  --preseqlen 32    --lr 2e-4    
-
 python3 main.py --mode 'ft' --train-file './data/msra/train.txt' --val-file './data/msra/valid.txt'  --test-file './data/msra/test.txt'   --task 'msra'  --lr 2e-5   
- 
 python3 main.py --mode 'memory' --train-file './data/msra/train.txt' --val-file './data/msra/valid.txt'  --test-file './data/msra/test.txt'    --task 'msra'  --preseqlen 16  --ffn-module-size 16   --lr 1e-4   --memory-num 50
-
 python3 main.py  --mode 'adapter'  --train-file './data/msra/train.txt' --val-file './data/msra/valid.txt'  --test-file './data/msra/test.txt'     --task 'msra'  --ffn-module-size 32  --lr 1e-4   
-
 python3 main.py  --mode 'memory-ffn'  --train-file './data/msra/train.txt' --val-file './data/msra/valid.txt'  --test-file './data/msra/test.txt'   --task 'msra'  --ffn-module-size 32  --lr 1e-4    --memory-num 50
-
 python3 main.py  --mode 'mam_adapter'  --train-file './data/msra/train.txt' --val-file './data/msra/valid.txt'  --test-file './data/msra/test.txt'   --task 'msra'  --ffn-module-size 16  --lr 1e-4   --preseqlen 16
-
-
 python3 main.py  --mode 'bitfit'  --train-file './data/msra/train.txt' --val-file './data/msra/valid.txt'  --test-file './data/msra/test.txt'   --task 'msra'   --lr 1e-4   
